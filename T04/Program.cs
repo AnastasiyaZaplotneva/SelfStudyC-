c351275890ddfa959d19b7b4c493b108aa936405
Anastasiya Z <nastya_tim2201@mail.ru>
2022-10-30 16:24:10 +0300

    сохранили задачку на сортировку массива

---
+++ T04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Сортировка массива
+Console.Clear();
+int[] nums = { 54, 7, -41, 2, 4, -2, 89, 107, -5, 12 };
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.Write($"{nums[i]}  ");
+} 
+// сортировка
+int temp;
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    for (int j = i + 1; j < nums.Length; j++)
+    {
+        if (nums[i] > nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
+ 
+Console.WriteLine();
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.Write($"{nums[i]}  ");
+}
